221a5f9b1d0172921306378b7660a9485fabbb34
Saran0605 <saransoundharajan05@gmail.com>
2025-01-02 09:58:27 +0530

    conflict resolved

--- manager.php
+++ manager.php
@@ -1391,13 +1391,7 @@ $row_count7 = mysqli_num_rows($result7);
 
                                 <?php
                                 // Set default month as the current month if no input is provided
-                                $fromdate = "2024-12-26";
-                                $todate = date("Y-m-d");
-
-                                if(isset($_POST["worker_count"])){
-                                    $fromdate = $_POST["selectfromdate"];
-                                    $todate = $_POST["selecttodate"];
-                                }
+                                $selectedMonth = isset($_POST['selectmonth']) ? $_POST['selectmonth'] : date('m');
 
                                 // Fetch data based on the selected month
                                 $sql9 = "SELECT * FROM worker_details WHERE usertype ='worker' ";
@@ -1412,11 +1406,10 @@ $row_count7 = mysqli_num_rows($result7);
                                         <div class="table-responsive">
                                             <h5 class="card-title">Worker's Record</h5>
 
-                                            <form id="worker_track">
-                                                <label for="selectfromdate">Select From Date: </label>
-                                                <input type="date" name="selectfromdate" value="<?php echo $selectedMonth; ?>" required>
-                                                <label for="selecttodate">Select To Date: </label>
-                                                <input type="date" name="selecttodate" value="<?php echo $selectedMonth; ?>" required>
+                                            <form method="POST" id="datesubmit">
+                                                <label for="selectmonth">Select Start and end Date</label>
+                                                <input type="date" name="fromdate">
+                                                <input type="date" name="todate">
                                                 <button type="submit" class="btn btn-primary">Enter</button>
                                             </form><span style="float:right">
                                                 <button id="download1" class="btn btn-success">Download as Excel</button></span><br><br>
@@ -3082,34 +3075,11 @@ $row_count7 = mysqli_num_rows($result7);
                 });
 
 
-                //to count the worker task based on date
-                $(document).on("submit","#worker_track",function(e){
-                    e.preventDefault();
-                    var data = new FormData(this);
-                    console.log(data);
-                    data.append("worker_count",true);
-                    $.ajax({
-                        type:"POST",
-                        url:"manager.php",
-                        data:data,
-                        contentType:false,
-                        processData:false,
-                        success:function(response){
-                            console.log(response);
-                            var res = jQuery.parseJSON(response);
-                            if(res.status == 200){
-                                console.log("success");
-                            }
-                            else{
-                                console.log("error");
-                            }
-                        }
-                    })
-                })
-
-                $(document).on('click', '.emailbtn', function(e) {
 
 
+
+
+                $(document),on("click","#datesubmit",function(e){
                     e.preventDefault();
                     var form = new FormData(this);
                     form.append("date",true);
