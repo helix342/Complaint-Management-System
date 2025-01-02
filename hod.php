62760a74a4fa4320e430b25fe2be48078265c661
Ragulraja47 <cragulraja2004@gmail.com>
2025-01-02 16:19:12 +0530

    some chages on hod

--- hod.php
+++ hod.php
@@ -1035,74 +1035,82 @@ $result3 = mysqli_query($conn, $sql3);
     </div>
 
     <!-- Raise Complaint Modal -->
-    <div id="cmodal" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
+    <div class="modal fade" id="cmodal" tabindex="-1"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header"
-                    style="background: linear-gradient(to bottom right, #cc66ff 1%, #0033cc 100%); color: white;">
-                    <h5 class="modal-title">Raise Complaint</h5>
-                    <button type="button" class="close" data-dismiss="modal"
+                <div class="modal-header" style="background:linear-gradient(to bottom right, #cc66ff 1%, #0033cc 100%);background-color:#7460ee;">
+                    <h5 class="modal-title" id="exampleModalLabel">Raise Complaint</h5>
+                    <button class="spbutton" type="button" class="btn-close" data-bs-dismiss="modal"
                         aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
                 </div>
-                <form id="addnewuser" enctype="multipart/form-data" onsubmit="handleSubmit(event)">
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <input type="hidden" id="hidden_faculty_id" value="<?php echo $_SESSION['faculty_id']; ?>">
-                            <input type="hidden" class="form-control" name="faculty_id" id="faculty_id" value="<?php echo $_SESSION['faculty_id']; ?>" readonly>
-                        </div>
+                <div>
+                    <form id="addnewuser" enctype="multipart/form-data" onsubmit="handleSubmit(event)">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <input type="hidden" id="hidden_faculty_id" value="<?php echo $_SESSION['faculty_id']; ?>">
+                                <input type="hidden" class="form-control" name="faculty_id" id="faculty_id" value="<?php echo $_SESSION['faculty_id']; ?>" readonly>
+                            </div>
+                            <div class="form-group" style="margin-bottom: 15px;">
+                                <label for="faculty" class="font-weight-bold" style="display: block; margin-bottom: 5px;">Choose Faculty <span style="color: red;">*</span></label>
+                                <select class="form-control" name="cfaculty" id="cfaculty" style="width: 100%; height: 40px; border-radius: 4px; border: 1px solid #ccc;">
+                                </select>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="block" class="form-label">Block <span style="color: red;">*</span></label>
-                            <input type="text" class="form-control" name="block_venue" placeholder="Eg:RK-206" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="venue" class="form-label">Venue <span style="color: red;">*</span></label>
-                            <select id="dropdown" class="form-control" name="venue_name" onchange="checkIfOthers()">
-                                <option>Select</option>
-                                <option value="class">Class Room</option>
-                                <option value="department">Department</option>
-                                <option value="lab">Lab</option>
-                                <option value="staff_room">Staff Room</option>
-                                <option id="oth" value="Other">Others</option>
-                            </select>
-                        </div>
+                            <div class="mb-3">
+                                <label for="type_of_problem" class="form-label">Type of Problem <span style="color: red;">*</span></label>
+                                <select class="form-control" name="type_of_problem" style="width: 100%; height:36px;">
+                                    <option>Select</option>
+                                    <option value="elecrtical">ELECTRICAL</option>
+                                    <option value="civil">CIVIL</option>
+                                    <option value="itkm">IT INFRA </option>
+                                    <option value="transport">TRANSPORT</option>
+                                    <option value="house">HOUSE KEEPING </option>
+                                </select>
+                            </div>
 
-                        <div id="othersInput" style="display: none;">
-                            <label class="form-label" for="otherValue">Please specify: <span style="color: red;">*</span></label>
-                            <input class="form-control" type="text" id="otherValue" name="otherValue">
-                        </div>
+                            <div class="mb-3">
+                                <label for="block" class="form-label">Block <span style="color: red;">*</span></label>
+                                <input type="text" class="form-control" name="block_venue" placeholder="Eg:RK-206 / Transport:Bus No" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="venue" class="form-label">Venue <span style="color: red;">*</span></label>
+                                <select id="dropdown" class="form-control" name="venue_name" onchange="checkIfOthers()"
+                                    style="width: 100%; height:36px;">
+                                    <option>Select</option>
+                                    <option value="class">Class Room</option>
+                                    <option value="department">Department</option>
+                                    <option value="lab">Lab</option>
+                                    <option value="staff_room">Staff Room</option>
+                                    <option id="oth" value="Other">Others</option>
+                                </select>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="type_of_problem" class="form-label">Type of Problem <span style="color: red;">*</span></label>
-                            <select class="form-control" name="type_of_problem">
-                                <option>Select</option>
-                                <option value="elecrtical">ELECTRICAL</option>
-                                <option value="civil">CIVIL</option>
-                                <option value="itkm">IT INFRA</option>
-                                <option value="transport">TRANSPORT</option>
-                                <option value="house">HOUSE KEEPING</option>
-                            </select>
+                            <div id="othersInput" style="display: none;">
+                                <label class="form-label" for="otherValue">Please specify: <span style="color: red;">*</span></label>
+                                <input class="form-control" type="text" id="otherValue" name="otherValue"> <br>
+                            </div>
+
+
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Problem Description <span style="color: red;">*</span></label>
+                                <input type="text" class="form-control" name="problem_description" placeholder="Enter Description" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="images" class="form-label">Image <span style="color: red;">*</span> </label>
+                                <input type="file" class="form-control" name="images" id="images" onchange="validateSize(this)" required>
+                            </div>
+                            <div class="mb-3">
+                                <input type="hidden" class="form-control" name="date_of_reg" id="date_of_reg" required>
+                            </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="description" class="form-label">Problem Description <span style="color: red;">*</span></label>
-                            <input type="text" class="form-control" name="problem_description" placeholder="Enter Description" required>
+                        <input type="hidden" name="status" value="2">
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
-                        <div class="mb-3">
-                            <label for="images" class="form-label">Image <span style="color: red;">*</span> </label>
-                            <input type="file" class="form-control" name="images" id="images" onchange="validateSize(this)" required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="hidden" class="form-control" name="date_of_reg" id="date_of_reg" required>
-                        </div>
-                    </div>
-                    <input type="hidden" name="status" value="2">
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
@@ -1216,6 +1224,37 @@ $result3 = mysqli_query($conn, $sql3);
                 swal("File is too large, Maximum 2 MB is allowed", "", "error");
                 $(input).val('');
             }
+        }
+
+        //raise complaint others field
+        function checkIfOthers() {
+            const dropdown = document.getElementById('dropdown');
+            const othersInput = document.getElementById('othersInput');
+
+            // Show the input field if "Others" is selected
+            if (dropdown.value === 'Other') {
+                othersInput.style.display = 'block';
+            } else {
+                othersInput.style.display = 'none';
+            }
+        }
+
+        function handleSubmit(event) {
+            event.preventDefault(); // Prevent form submission for demo purposes
+            const dropdown = document.getElementById('dropdown');
+            const selectedValue = dropdown.value;
+            let finalValue;
+
+            // Get the appropriate value based on the dropdown selection
+            if (selectedValue === 'Other') {
+                finalValue = document.getElementById('otherValue').value;
+            } else {
+                finalValue = selectedValue;
+            }
+
+            console.log("Selected Category:", finalValue);
+            // You can then send this data to the backend or process it further
+            $("#oth").val(finalValue);
         }
     </script>
 
@@ -1473,7 +1512,7 @@ $result3 = mysqli_query($conn, $sql3);
         $(document).on('click', '#facultyinfo', function(e) {
             e.preventDefault();
             var user_id = $(this).val();
-            var fac_id = $("#facultyinfo").data("value"); 
+            var fac_id = $("#facultyinfo").data("value");
 
             console.log(user_id);
             console.log(fac_id);
@@ -1483,7 +1522,7 @@ $result3 = mysqli_query($conn, $sql3);
                 data: {
                     'facultydetails': true,
                     'user_id': user_id,
-                    'fac_id':fac_id
+                    'fac_id': fac_id
                 },
                 success: function(response) {
                     var res = jQuery.parseJSON(response);
